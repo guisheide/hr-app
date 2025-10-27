58df5ae3d399ca1c921250514735bd3ae530464f
Guilherme Barbosa <gui.lplb@gmail.com>
2025-10-27 03:27:32 -0300

    feat: Implement designation editing functionality with validation and department retrieval

--- app/Livewire/Admin/Designations/Edit.php
+++ app/Livewire/Admin/Designations/Edit.php
@@ -2,12 +2,39 @@
 
 namespace App\Livewire\Admin\Designations;
 
+use App\Models\Department;
+use App\Models\Designation;
 use Livewire\Component;
 
 class Edit extends Component
 {
+
+    public $designation;
+
+    public function rules()
+    {
+        return [
+            'designation.name' => 'required|string|max:255',
+            'designation.department_id' => 'required|exists:departments,id',
+        ];
+    }
+
+    public function mount($id){
+        $this->designation = Designation::find($id);
+    }
+
+      public function save(){
+        $this->validate();
+        $this->designation->save();
+        session()->flash('sucess', 'Designation edited successfully');
+        return $this->redirectIntended(route('designations.index', true));
+    }
+
+
     public function render()
     {
-        return view('livewire.admin.designations.edit');
+        return view('livewire.admin.designations.edit', [
+            'departments' => Department::inCompany()->get()
+        ]);
     }
 }
